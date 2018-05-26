6ee4c1c476756066352847138d93bc885899ee95
Wanhar Aderta <wanhardaengmaro@gmail.com>
2018-05-26 11:07:10 +0700

    update xml

---
+++ app/src/main/java/id/kat/pokemon/view/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package id.kat.pokemon.view.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import id.kat.pokemon.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
